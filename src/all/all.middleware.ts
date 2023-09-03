5ecf140d31c7ddae5793ec2552104a8de9aef589
bhwa233 <404174262@qq.com>
2023-09-03 22:23:50 +0800

    跨域

--- src/all/all.middleware.ts
+++
@@ -1,4 +0,0 @@
-export function AllMiddleware(req: any, res: any, next: any) {
-  console.log('我是全局中间件.....');
-  next();
-}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
-import { AllMiddleware } from './all/all.middleware';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+
+function MiddleWareAll(req: any, res: any, next: any) {
+  console.log('我是全局中间件.....');
+  // res.send('禁止访问')
+  next();
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.use(AllMiddleware);
+  app.use(cors());
+  app.use(MiddleWareAll);
   await app.listen(6060);
 }
 bootstrap();
