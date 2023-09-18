3a15ee264505bc5a891d1615eb088961c66fe611
bhwa233 <404174262@qq.com>
2023-09-18 20:20:37 +0800

    处理cors

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.setGlobalPrefix('api');
-  app.use(cors());
   await app.listen(6060).then(() => {
-    new Logger('NestApplication').log(
-      'Server is running on http://localhost:6060',
-    );
+    new Logger('NestApplication').log('Server is running ');
   });
 }
 bootstrap();
