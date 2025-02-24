6a0219fb1d4c36faada535ad64212703d697b07c
bhwa233 <404174262@qq.com>
2025-02-24 14:13:38 +0800

    更新期货API URL，优化数据获取逻辑，调整请求头配置

--- src/feature/command/acions/future.ts
+++ src/feature/command/acions/future.ts
@@ -1,19 +1,19 @@
 import axios from "axios";
 import { decode } from "querystring";
 
-const Future_API_URL = 'http://hq.sinajs.cn/' // Replace with your actual API URL
+const Future_API_URL = 'http://w.sinajs.cn/' // Replace with your actual API URL
 const SUGGESTION_API_URL = 'http://suggest3.sinajs.cn/suggest/' // Replace with your actual API URL
 
 // "var hq_str_hf_XAU=\"2363.44,2356.800,2363.44,2363.79,2366.23,2354.11,15:30:00,2356.80,2356.45,0,0,0,2024-07-05,伦敦金（现货黄金）\";\n"
 function extractPrices(hq_str: string, symbol: string) {
     const match = hq_str.match(/(?:"[^"]*")/);
     if (!match) return `获取${symbol}数据失败`;
-    
+
     const data = match[0].slice(1, -1).split(',');
     const name = data.find(item => /[\u4e00-\u9fa5]/.test(item));
     const currentPrice = parseFloat(data[0]);
     const prePrice = parseFloat(data[8]);
-    
+
     if (isNaN(currentPrice) || isNaN(prePrice)) {
         return `${symbol}数据格式错误`;
     }
@@ -29,14 +29,6 @@ export async function getFutureSuggest(searchText = 'XAU'): Promise<string> {
                 type: '85,86,88',
                 key: encodeURIComponent(searchText)
             }
-            // responseType: 'arraybuffer',
-            // transformResponse: [
-            //     (data) => {
-            //         const body = decode(data, 'GB18030');
-            //         return body;
-            //     },
-            // ],
-            // headers: randHeader(),
         });
         const text = futureResponse.data.slice(18, -2)
         if (text === '') {
@@ -71,16 +63,15 @@ export async function getFutureData(symbol: string): Promise<string> {
             responseType: 'arraybuffer',
             transformResponse: [
                 (data) => {
-                    const body = decode(data, 'GB18030');
-                    return body;
+                    const decoder = new TextDecoder('GB18030');
+                    return decoder.decode(data);
                 },
             ],
             params: {
                 list: symbol,
             },
             headers: {
-                // ...randHeader(),
-                Referer: 'http://finance.sina.com.cn/',
+                Referer: 'https://gu.sina.cn/ft/hq/hf.php',
             },
         })
 
