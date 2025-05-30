7b7257789fdf0b8c73da5f81278bd90cec9c89e1
bhwa233 <404174262@qq.com>
2025-05-30 15:56:19 +0800

    refactor: 移除页面抓取模块及相关DTO，简化应用结构

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { AiModule } from './feature/ai/ai.module';
 import { ConfigModule } from '@nestjs/config';
 import { FishingTimeModule } from './feature/fishing-time/fishing-time.module';
 import { StockMarketModule } from './feature/stock-market/stock-market.module';
-import { PageScraperModule } from './feature/page-scraper/page-scraper.module';
 import { CommandModule } from './feature/command/command.module';
 
 @Module({
@@ -16,7 +15,6 @@ import { CommandModule } from './feature/command/command.module';
     FishingTimeModule,
     AiModule,
     StockMarketModule,
-    PageScraperModule,
     CommandModule, 
   ],
   controllers: [AppController],
